4ea224802defed210e2ceb05c2c5f601aa17ea03
Lukas Spettel <70133373+lukasspettel@users.noreply.github.com>
2023-03-23 11:46:02 +0100

    add pocketbase url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	import Navbar from '$lib/components/Navbar.svelte'
 	import bgImage from '$lib/assets/images/background/Topographic Map Patterns.svg'
 
-	pbStore.set('http://195.201.225.162:8090/')
+	pbStore.set('https://pocketbase.telesis.at/')
 </script>
 
 <background style="background-image: url('{bgImage}')" />
